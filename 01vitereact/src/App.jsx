482809f0f8d73f5b8686c212c814e13de769acce
unknown <sharmakeshav54126@gmail.com>
2023-11-13 17:24:12 +0530

    viteReact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Chai/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chai(){
+    return(
+        <>
+            <h1>Chai and backchodi</h1>
+        </>
+    )
+}
+
+export default Chai
